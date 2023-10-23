1bf5d2da946c275261b6dda7ad55d22902bbb544
k_95.21 <kohta0421kochi@gmail.com>
2023-10-23 19:06:46 +0900

    add all files

---
+++ customhook/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Post from './components/Post';
+import User from './components/User';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Custom Hook Practice</h1>
+      <User />
+      <Post />
+    </div>
+  );
+}
+
+export default App;
